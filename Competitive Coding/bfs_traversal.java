231f023931e9543493433fafeb1295060f9176b3
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-10-21 12:23:45 +0530

    Add files via upload

--- Competitive Coding/bfs_traversal.java
+++ Competitive Coding/bfs_traversal.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class bfs_traversal {
 
@@ -11,20 +11,28 @@ public class bfs_traversal {
         }
     }
 
-    public static void depth_first_search(int[][] matrix, boolean[] visited, int vertex){
-        System.out.print(vertex + " ");
-        visited[vertex] = true;
-        for(int i=0;i<matrix.length;i++){
-            if(matrix[vertex][i] == 1 && !visited[i]){
-                depth_first_search(matrix, visited, i);
+    public static void breadth_first_traversal(int[][] matrix, boolean[] visited, int vertex){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(vertex);
+
+        while(!queue.isEmpty()){
+            int vertex_ = queue.poll();
+            System.out.print(vertex_ + " ");
+            visited[vertex_] = true;
+
+            for(int i=0;i<matrix.length;i++){
+                if(matrix[vertex_][i] == 1 && !visited[i]){
+                    visited[i] = true;
+                    queue.add(i);
+
+                }
             }
         }
+
     }
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter number of vertext: ");
+        System.out.print("Enter number of vertices: ");
         int vertices = sc.nextInt();
         System.out.print("Enter number of edges: ");
         int edges = sc.nextInt();
@@ -40,16 +48,12 @@ public class bfs_traversal {
 
             matrix[source][destination] = 1;
             matrix[destination][source] = 1;
-
         }
 
-        System.out.print("DFS Traversal: ");
-        depth_first_search(matrix, visited, 0);
-        System.out.println("\nMatrix Display: ");
-
+        System.out.print("BFS Traversal: ");
+        breadth_first_traversal(matrix, visited, 0);
+        System.out.println("\nMatrix:");
         printMatrix(matrix);
 
-
-        
     }
 }
