e358e0f49ce9793d3ea1fb75f7f7c3638ef6ec12
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2025-02-05 23:15:19 +0530

    Add files via upload

---
+++ trie_question1.java
@@ -0,0 +1,104 @@
+public class trie_question1 {
+    public static void main(String[] args) {
+
+        // String str = "mad";
+        String[] words = {"madhav","m","ma","mad","madh","madha"};
+
+        Trie root = new Trie();
+        
+        for(String str_ : words){
+            insertInTrie(root, str_);
+        }
+
+        // System.out.println(startsWith(root, str));
+
+        StringBuilder maxPrefixString = new StringBuilder();
+
+        longestPrefix(root, new StringBuilder(), maxPrefixString);
+
+        System.out.println(maxPrefixString);
+
+
+
+        
+    }
+    static class Trie{
+        boolean endsofword;
+        Trie[] children;
+
+        Trie(){
+            children = new Trie[26];
+            for(int i=0;i<26;i++){
+                children[i] = null;
+            }
+        }
+    }
+
+
+    public static void insertInTrie(Trie root, String str){
+        Trie current = root;
+        for(int i=0;i<str.length();i++){
+            int index = str.charAt(i) - 'a';
+            if(current.children[index] == null){
+                current.children[index] = new Trie();
+            }
+            if(i == str.length() - 1){
+                current.children[index].endsofword = true;
+            }
+            current = current.children[index];
+        }
+    }
+
+
+    public static boolean searchInTrie(Trie root, String str){
+        Trie current = root;
+
+        for(int i=0;i<str.length();i++){
+            int index = str.charAt(i) - 'a';
+            if(current.children[index] == null){
+                return false;
+            }
+            if(i == str.length() - 1 && current.children[index].endsofword == false){
+                return false;
+            }
+
+            current = current.children[index];
+        }
+        return true;
+    }
+
+    public static boolean startsWith(Trie root, String str){
+
+        Trie current = root;
+
+        for(int i=0;i<str.length();i++){
+            int index = str.charAt(i) - 'a';
+            if(current.children[index] == null){
+                return false;
+            }
+
+            current = current.children[index];
+        }
+        return true;
+    }
+
+    public static void longestPrefix(Trie root, StringBuilder temp, StringBuilder maxPrefix){
+        Trie current = root;
+        if(current == null){
+            return;
+        }
+
+        for(int i=0;i<26;i++){
+            if(current.children[i] != null && current.children[i].endsofword == true){
+                temp.append((char)(i + 'a'));
+                if(temp.length() > maxPrefix.length()){
+                    maxPrefix.setLength(0);
+                    maxPrefix.append(temp);
+                }
+                longestPrefix(current.children[i], temp, maxPrefix);
+                
+                temp.deleteCharAt(temp.length() - 1);
+            }
+        }
+    }
+}
