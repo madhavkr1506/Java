c70101c82e417377311bf2f89c44608894eeb2ac
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2025-03-12 10:23:33 +0530

    Add files via upload

---
+++ Practice3.java
@@ -0,0 +1,19 @@
+public class Practice3 {
+    public static void main(String[] args) {
+        int[] cost = {10, 15, 20};
+        System.out.println(minCostClimbingStairs(cost));
+    }
+
+    public static int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i=2;i<n;i++){
+            dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+        }
+
+        return Math.min(dp[n-1], dp[n-2]);
+    }
+}
