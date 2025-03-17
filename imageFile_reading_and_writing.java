05ae001aed94a63dbc9a92b27e1e78cf6c802123
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2025-03-17 23:03:46 +0530

    Add files via upload

--- imageFile_reading_and_writing.java
+++ imageFile_reading_and_writing.java
@@ -1,10 +1,13 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOError;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class imageFile_reading_and_writing {
+    private static Logger LOGGER = Logger.getLogger(imageFile_reading_and_writing.class.getName());
     public static void main(String[] args) {
+        LOGGER.log(Level.INFO,"Reading and Writing into a file.");
         try{
             File file = new File("D:\\Question\\50DaysCoding.png");
             FileInputStream inputStram = new FileInputStream(file);
@@ -14,23 +17,16 @@ public class imageFile_reading_and_writing {
 
             inputStram.close();
 
-
-            System.out.println("Image file has been read. Total bytes: " + number_of_bytes);
-
+            LOGGER.log(Level.INFO, "Image file has been read till " + number_of_bytes + " bytes");
 
             FileOutputStream outputStream = new FileOutputStream("D:\\Question\\output.png");
             outputStream.write(bytes);
             outputStream.close();
 
-            System.out.println("Output has been created.");
-
-
-
+            LOGGER.log(Level.INFO, "Image file has been written till " + number_of_bytes + " bytes");
 
         }catch(Exception e){
-            e.printStackTrace();
-        }catch(IOError e){
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "An error has been occured while reading and writing in a file");
         }
     }
 }
