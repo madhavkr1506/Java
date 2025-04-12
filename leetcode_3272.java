843df1e0f724b19d6cd464b34c3e8e5fd930caeb
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2025-04-12 21:48:00 +0530

    Add files via upload

--- leetcode_3272.java
+++ leetcode_3272.java
@@ -1,47 +1,81 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class leetcode_3272 {
+
     public static void main(String[] args) {
-        System.out.println(countGoodIntegers(3, 5)); 
+        int good_integer = count_good_integer(3, 5);
+        System.out.println(good_integer);
+
     }
-    public static long countGoodIntegers(int n, int k) {
-        long[] fac = new long[n + 1];
-        fac[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            fac[i] = fac[i - 1] * i;
+
+
+    public static int count_good_integer(int n, int k){
+        int lower_bound = n_digit_start(n);
+        int upper_bound = n_digit_last(n);
+
+        int count = 0;
+
+        for(int i=lower_bound;i<=upper_bound;i++){
+            String str = Integer.toString(i);
+            List<String> all_perms = find_all_perms(str);
+            for (String prem : all_perms){
+                if(prem.charAt(0) == '0'){
+                    continue;
+                }
+                if(chack_palindrome(prem) && Integer.parseInt(prem) % k == 0){
+                    count++;
+                    break;
+                }
+            }
+
+        }
+        return count;
+    }
+
+
+    public static boolean chack_palindrome(String str){
+        int left = 0;
+        int right = str.length() - 1;
+        while(left < right){
+            if(str.charAt(left++) != str.charAt(right--)) return false;
+        }
+        return true;
+    }
+
+
+
+    public static List<String> find_all_perms(String str){      
+        List<String> all_prems = new ArrayList<>();
+        all_permutations(str, "", new boolean[str.length()], all_prems);    
+        return all_prems;
+    }
+
+    public static void all_permutations(String str, String temp,boolean[] used, List<String> all_perms){
+        if(temp.length() == str.length()){
+            all_perms.add(temp);
+            return;
         }
 
-        long ans = 0;
-        Set<String> vis = new HashSet<>();
-        int base = (int) Math.pow(10, (n - 1) / 2);
-
-        for (int i = base; i < base * 10; i++) {
-            String s = String.valueOf(i);
-            StringBuilder sb = new StringBuilder(s).reverse();
-            s += sb.substring(n % 2);
-            if (Long.parseLong(s) % k != 0) {
-                continue;
+        for (int i = 0; i < str.length(); i++) {
+            if(!used[i]){
+                used[i] = true;
+                all_permutations(str, temp + str.charAt(i), used, all_perms);
+                used[i] = false;
             }
-
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String t = new String(arr);
-            if (vis.contains(t)) {
-                continue;
-            }
-            vis.add(t);
-            int[] cnt = new int[10];
-            for (char c : arr) {
-                cnt[c - '0']++;
-            }
-
-            long res = (n - cnt[0]) * fac[n - 1];
-            for (int x : cnt) {
-                res /= fac[x];
-            }
-            ans += res;
+            
         }
-
-        return ans;
     }
+
+    public static int n_digit_start(int digit){
+        return (int)Math.pow(10, digit - 1);
+    }
+
+    public static int n_digit_last(int digit){
+        return (int)Math.pow(10, digit) - 1;
+    }
+    
 }
+
+
+
