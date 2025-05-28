dac27173b68562a360f1781316a62fef9b7a9cff
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2025-05-28 21:12:54 +0530

    Add files via upload

--- leetcode_785.java
+++ leetcode_785.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class leetcode_785 {
     public static void main(String[] args) {
         int[][] graph = new int[][]{{1,2,3},{0,2},{0,1,3},{0,2}};
@@ -10,7 +13,7 @@ public class leetcode_785 {
         int[] visit_state = new int[number_of_nodes];
         for(int i=0;i<graph.length;i++){
             if(visit_state[i] == 0){
-                if(!dfs_call(graph, visit_state, i, 1)){
+                if(!bfs_call(graph, visit_state, i, 1)){
                     return false;
                 }
             }
@@ -42,5 +45,32 @@ public class leetcode_785 {
         
         return true;
 
+    }
+
+
+    public static boolean bfs_call(int[][] graph, int[] visit_state, int current_node, int color){
+        try{
+            visit_state[current_node] = color;
+            Queue<Integer> queue = new LinkedList<>();
+            queue.offer(current_node);
+
+            while(!queue.isEmpty()){
+                current_node = queue.poll();
+                for(int i=0;i<graph[current_node].length;i++){
+                    int neighbor = graph[current_node][i];
+                    if(visit_state[neighbor] == 0){
+                        visit_state[neighbor] = -visit_state[current_node];
+                        queue.offer(neighbor);
+                    }else if(visit_state[neighbor] == visit_state[current_node]){
+                        return false;
+                    }
+                } 
+            }
+
+            return true;
+        }catch(Exception e){
+            System.err.println("Exception: " + e.getMessage());
+            return false;
+        }
     }
 }
